505e8f0e9f60443e2354e40e313ee2f4ca218399
filipe-gomes <filipegomes404@gmail.com>
2020-08-05 22:14:40 -0500

    added authentication to app.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,36 @@ import HomePage from './views/homepage/homepage.component';
 import ShopPage from './views/shop/shop.component';
 import SignInAndSignUp from './views/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-  <div>
-  <Header />
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-      <Route exact path='/signin' component={SignInAndSignUp} />
-    </Switch>
-  </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+      <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SignInAndSignUp} />
+        </Switch>
+      </div>
+      );
+  }
+  
 }
 
 export default App;
